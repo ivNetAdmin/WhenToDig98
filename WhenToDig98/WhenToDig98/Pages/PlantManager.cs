846a2a868f06ca8fcd89f750a7d192bf8fbca0c2
ivNetAdmin <bp@ivnet.co.uk>
2016-05-17 10:54:30 +0100

    Update PlantManager.cs
    Plant redesign

--- WhenToDig98/WhenToDig98/Pages/PlantManager.cs
+++ WhenToDig98/WhenToDig98/Pages/PlantManager.cs
@@ -17,14 +17,14 @@ namespace WhenToDig98.Pages
 
             Padding = new Thickness(10);
 
-            var grid = BuildForm();
+           // var grid = BuildForm();
 
             this.Content = new StackLayout
             {
                 Padding = new Thickness(5, 10),
                 VerticalOptions = LayoutOptions.StartAndExpand,
                 Children = {
-                    grid
+                   // grid
                 }
             };
         }
@@ -33,107 +33,6 @@ namespace WhenToDig98.Pages
         {
             base.OnAppearing();
             PageToolBarItems.Build(_database, this);           
-        }
-
-
-        private Grid BuildForm()
-        {
-            Grid grid = new Grid
-            {
-                VerticalOptions = LayoutOptions.Fill
-            };
-
-            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-            grid.Children.Add(new Label
-            {
-                Text = "Name",
-                TextColor = Color.Silver,
-                HorizontalTextAlignment = TextAlignment.Start,
-                VerticalTextAlignment = TextAlignment.Center
-            }, 0, 0);
-
-            grid.Children.Add(new Editor
-            {
-                //Text = _task == null ? string.Empty : _task.Description
-            }, 1, 0);
-            Grid.SetColumnSpan(grid.Children[grid.Children.Count - 1], 6);
-
-            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-            grid.Children.Add(new Label
-            {
-                Text = "Type",
-                TextColor = Color.Silver,
-                HorizontalTextAlignment = TextAlignment.Start,
-                VerticalTextAlignment = TextAlignment.Center
-            }, 0, 1);
-
-            grid.Children.Add(new Editor
-            {
-                //Text = _task == null ? string.Empty : _task.Description
-            }, 1, 1);
-            Grid.SetColumnSpan(grid.Children[grid.Children.Count - 1], 6);
-
-            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-            grid.Children.Add(new Label
-            {
-                Text = "P-Time",
-                TextColor = Color.Silver,
-                HorizontalTextAlignment = TextAlignment.Start,
-                VerticalTextAlignment = TextAlignment.Center
-            }, 0, 2);
-
-            grid.Children.Add(new Editor
-            {
-                //Text = _task == null ? string.Empty : _task.Description
-            }, 1, 2);
-            Grid.SetColumnSpan(grid.Children[grid.Children.Count - 1], 6);
-            
-            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-             grid.Children.Add(new Label
-            {
-                Text = "H-Time",
-                TextColor = Color.Silver,
-                HorizontalTextAlignment = TextAlignment.Start,
-                VerticalTextAlignment = TextAlignment.Center
-            }, 0, 3);
-
-            grid.Children.Add(new Editor
-            {
-                //Text = _task == null ? string.Empty : _task.Description
-            }, 1, 3);
-            Grid.SetColumnSpan(grid.Children[grid.Children.Count - 1], 6);
-            
-            grid.Children.Add(new Label
-            {
-                Text = "P-Note",
-                TextColor = Color.Silver,
-                HorizontalTextAlignment = TextAlignment.Start,
-                VerticalTextAlignment = TextAlignment.Center
-            }, 0, 4);
-
-            grid.Children.Add(new Editor
-            {
-               // Text = _task == null ? string.Empty : _task.Notes
-            }, 1, 4);
-            Grid.SetColumnSpan(grid.Children[grid.Children.Count - 1], 6);
-            Grid.SetRowSpan(grid.Children[grid.Children.Count - 1], 2);
-
-            grid.Children.Add(new Label
-            {
-                Text = "H-Note",
-                TextColor = Color.Silver,
-                HorizontalTextAlignment = TextAlignment.Start,
-                VerticalTextAlignment = TextAlignment.Center
-            }, 0, 6);
-
-            grid.Children.Add(new Editor
-            {
-                // Text = _task == null ? string.Empty : _task.Notes
-            }, 1, 6);
-            Grid.SetColumnSpan(grid.Children[grid.Children.Count - 1], 6);
-            Grid.SetRowSpan(grid.Children[grid.Children.Count - 1], 2);
-            return grid;
-
         }
     }
 }
