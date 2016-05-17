e37eea2f02b78d06991a884f6c224f37d6e5c552
ivNetAdmin <bp@ivnet.co.uk>
2016-05-17 14:54:14 +0100

    Update Variety.cs
    Plant manager functionality

--- WhenToDig98/WhenToDig98/Models/Variety.cs
+++ WhenToDig98/WhenToDig98/Models/Variety.cs
@@ -7,6 +7,7 @@ namespace WhenToDig98.Models
     {
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
+        public int PlantID { get; set; }
         public string Name { get; set; }
         public string PlantingNotes { get; set; }
         public string HarvestingNotes { get; set; }
