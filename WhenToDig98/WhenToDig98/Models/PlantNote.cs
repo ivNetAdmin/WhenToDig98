776f11217c0f197d52cb42fd2da91d73f258b340
ivNetAdmin <bp@ivnet.co.uk>
2016-05-27 08:57:34 +0100

    Update PlantNote.cs
    Review functionality

--- WhenToDig98/WhenToDig98/Models/PlantNote.cs
+++ WhenToDig98/WhenToDig98/Models/PlantNote.cs
@@ -4,6 +4,8 @@ namespace WhenToDig98.Models
     public class PlantNote
     {
         public string PlantName { get; set; }
+        public string PlantType { get; set; }
+        public string Variety { get; set; }
         public string Planting { get; set; }
         public string Harvesting { get; set; }
     }
