fb67ddfb613177cde105a790f4b191ef2114ba2a
ivNetAdmin <bp@ivnet.co.uk>
2016-05-27 09:16:56 +0100

    Update Review.cs
    Review functionality

--- WhenToDig98/WhenToDig98/Pages/Review.cs
+++ WhenToDig98/WhenToDig98/Pages/Review.cs
@@ -126,20 +126,24 @@ namespace WhenToDig98.Pages
             _noteGrid.Children.Clear();
 
             var rowCounter = 0;
+            var plant = string.Empty;
+            
             foreach(var note in _notes)
             {
-                _noteGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-
                 var notes = note.Split("-");
-                
-                _noteGrid.Children.Add(new Label
+                if(plant == notes[0])
                 {
-                    Text = notes[0],
-                    TextColor = Color.Teal,
-                    HorizontalTextAlignment = TextAlignment.Start,
-                    VerticalTextAlignment = TextAlignment.Center
-                }, 0, rowCounter);
-                _noteGrid.SetColumnSpan(grid.Children[grid.Children.Count - 1], 3);
+                    _noteGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });   
+                    _noteGrid.Children.Add(new Label
+                    {
+                        Text = notes[0],
+                        TextColor = Color.Teal,
+                        HorizontalTextAlignment = TextAlignment.Start,
+                        VerticalTextAlignment = TextAlignment.Center
+                    }, 0, rowCounter);
+                    _noteGrid.SetColumnSpan(grid.Children[grid.Children.Count - 1], 3);
+                 }
+                plant = notes[0];
                 
                 _noteGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
 
