1340a3e0898e5a55d073f0e98d0e76658d94f5af
ivNetAdmin <bp@ivnet.co.uk>
2016-05-27 08:59:53 +0100

    Update PlantNote.cs
    Review functionality

--- WhenToDig98/WhenToDig98/Models/PlantNote.cs
+++ WhenToDig98/WhenToDig98/Models/PlantNote.cs
@@ -8,5 +8,16 @@ namespace WhenToDig98.Models
         public string Variety { get; set; }
         public string Planting { get; set; }
         public string Harvesting { get; set; }
+        
+        public string PlantDisplayName
+        {
+            get {
+                return string.Format("{0}{1}{2}", 
+                this.PlantName, 
+                string.IsNullOrEmpty(this.PlantType) ? string.Empty : string.Format(" ({0})", this.PlantType)
+                string.IsNullOrEmpty(this.Variety) ? string.Empty : string.Format(" {0}", this.Variety)
+                );
+            }
+        }
     }
 }
